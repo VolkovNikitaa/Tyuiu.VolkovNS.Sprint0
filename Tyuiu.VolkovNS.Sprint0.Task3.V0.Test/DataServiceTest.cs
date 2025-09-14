20b01e021c78a86e7b38dd8c1342ea616726a8a6
Никита Волков <Tyuiu.VolkovNS@outlook.com>
2025-09-14 14:29:21 +0300

    Task3

---
+++ Tyuiu.VolkovNS.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.VolkovNS.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.VolkovNS.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckeValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
