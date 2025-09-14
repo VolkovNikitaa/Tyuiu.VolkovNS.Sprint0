0d0ce582946e5c2cf602b2f6454a8f4eb6f9cbfe
Никита Волков <Tyuiu.VolkovNS@outlook.com>
2025-09-14 21:25:49 +0300

    Task6

--- Tyuiu.VolkovNS.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.VolkovNS.Sprint0.Task6.V0/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.VolkovNS.Sprint0.Task6.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numsArray = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+        Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+        Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+        Console.ReadKey();
+    }
+}
